49840d42d58116b56015a07e97b41d6aa3493a9b
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-04-05 13:27:37 +0300

    Создать метод, на вход которому поступает адрес. Метод должен посчитать размер полученного файла или папки и отобразить это в нормальном виде (байтах, кб, мб, гб, тб. Округлять до двух символов после запятой).

--- HW28/src/Main.java
+++ HW28/src/Main.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 public class Main {
@@ -7,12 +11,23 @@ public class Main {
         String path = new Scanner(System.in).nextLine();
         printSize(path);
 
-        System.out.println("============= ДЗ 3 =============");
-        setSale();
+//        System.out.println("============= ДЗ 3 =============");
+//        setSale();
     }
 
     private static void printSize(String path) {
         //todo Дописать код расчета размера и корректного отображения
+        double sum = 0;
+        try {
+            sum = Files.walk(Path.of(path))
+                    .map(Path::toFile)
+                    .filter(File::isFile)
+                    .mapToDouble(File::length).sum();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.print(sum + " байт " + Math.round((sum / 1024) * 1000.0) / 1000.0 + " килобайт");
+
     }
 
     private static void setSale() {
